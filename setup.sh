92b6b063eda23d7c7ea7f0fb30decaf0b4aa8cc8
D Kumar <deepkumar0501@gmail.com>
2025-04-01 23:38:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
-email = \"calvinsamueldonkor@gmail.com\"\n\
+email = \"dkumar0501@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
-port = $PORT\n\
+port = \$PORT\n\
 " > ~/.streamlit/config.toml
