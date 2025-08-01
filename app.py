1d477aa11fed0aa56d1a9fc8bb581ecd9966b74d
D Kumar <deepkumar0501@gmail.com>
2025-08-01 12:01:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image, ImageOps  
-from img_classification import teachable_machine_classification 
+from img_classification import teachable_machine_classification  
 
 st.title("Breast Cancer Detection R&D D_Kumar IIT Patna")
 st.header("Ultrasound Cancer Image Upload")
