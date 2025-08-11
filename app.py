757702f78f134633c3e9a45a2313d8bae670f142
D Kumar <deepkumar0501@gmail.com>
2025-08-11 22:15:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.text("Is There Cancer In Your Body This Machine Will Tell ")
      
 uploaded_file = st.file_uploader("Choose a scan ...", type="png") 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
+    image = Image.open(uploaded_file) 
     st.image(image, caption='Uploaded Scan.', use_column_width=True)
     st.write("") 
     st.write("Classifying...")
@@ -20,6 +20,7 @@ if uploaded_file is not None:
         st.write("The Scan Is Malignant Cum Cancerous")
     else:
         st.write("The Scan Is Benign Cum Non Cancerous")
+
 
 
 
