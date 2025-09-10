305966310cee5ef7740e2214acf77c5364126435
D Kumar <deepkumar0501@gmail.com>
2025-09-10 22:47:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.header("Ultrasound Cancer Image Upload")
 st.text("Is There Cancer In Your Body This Machine Will Tell ")   
 
      
-uploaded_file = st.file_uploader("Choose a scan ...", type="png") 
+uploaded_file = st.file_uploader("Choose a scan ...", type="png")  
 if uploaded_file is not None:
     image = Image.open(uploaded_file) 
     st.image(image, caption='Uploaded Scan.', use_column_width=True)
@@ -20,6 +20,7 @@ if uploaded_file is not None:
         st.write("The Scan Is Malignant Cum Cancerous")
     else:
         st.write("The Scan Is Benign Cum Non Cancerous")
+
 
 
 
